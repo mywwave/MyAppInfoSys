f84ed04201e313b4bcbdbd97fbb4239c2358407e
mywwave@outlook.com <mywwave@outlook.com>
2024-01-18 15:59:43 +0300

    Complete task

---
+++ Checker/PcInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace Checker
+{
+    /// <summary>
+    /// Класс для получения информации о компьютере
+    /// </summary>
+    public class PcInfo
+    {
+        #region Методы      
+        public void PrintPCInfo()
+        {
+            string pcInfo = GetPCInfo();
+            Console.WriteLine(pcInfo);
+        }
+        #endregion
+
+        #region Получение строк
+        private string GetPCInfo()
+        {
+            string username = Environment.UserName;
+            string localIp = GetLocalIpAddress();
+
+            return $"Username: {username}\nLocal IP: {localIp}";
+        }
+
+        private string GetLocalIpAddress()
+        {
+            string localIp = "";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ipAddress in host.AddressList)
+            {
+                if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIp = ipAddress.ToString();
+                    break;
+                }
+            }
+            return localIp;
+        }
+        #endregion
+
+    }
+}
--- MyAppInfoSys/Program.cs
+++ MyAppInfoSys/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+public class Program
+{
+    public static void Main()
+    {
+        string assemblyPath = Path.Combine(Directory.GetCurrentDirectory(), "Checker.dll");
+        Assembly? assembly = Assembly.LoadFrom(assemblyPath);
+        if (assembly != null)
+        {
+            Type? type = assembly.GetType("Checker.PcInfo");
+            if (type != null)
+            {
+                object? instance = Activator.CreateInstance(type);
+                if (instance != null)
+                {
+                    MethodInfo? method = type.GetMethod("PrintPCInfo");
+                    method?.Invoke(instance, null);
+                }
+            }
+        }
+    }
+}
